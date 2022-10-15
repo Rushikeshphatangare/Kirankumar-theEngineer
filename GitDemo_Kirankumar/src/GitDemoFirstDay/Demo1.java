ca9874aaefe86b1a2b918cb4543accf3920eddfa
Rushikeshphatangare <115785111+Rushikeshphatangare@users.noreply.github.com>
2022-10-15 04:13:29 -0400

    Update Demo1.java

--- GitDemo_Kirankumar/src/GitDemoFirstDay/Demo1.java
+++ GitDemo_Kirankumar/src/GitDemoFirstDay/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1
 	public static void main(String[] args) 
 	{
 		System.out.println("It's me kirankumar GitHub");
+		System.out.println("It's me kirankumar GitHubby rushi");
 		System.out.println("It's me kirankumar GitHub Alpha ");
 		System.out.println("It's me kirankumar .......I am inevitable ");
 	}
